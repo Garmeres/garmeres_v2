f2c61c3c393ae841d4bef8891ff3a0c5de604ace
Leevi978 <levi.sorum@gmail.com>
2023-04-12 21:11:41 +0200

    style: 💄 Navigation items fade animation

--- src/components/layout/menu/menu-nav-item.js
+++ src/components/layout/menu/menu-nav-item.js
@@ -4,11 +4,35 @@ import { Link } from "gatsby";
 import variables from "../../../styles/variables";
 
 const NavItem = styled((props) => <Link {...props} />)`
-    color: inherit;
-    text-decoration: none;
+    color: var(--text-color-light);
+    text-decoration: underline solid transparent;
     font-size: var(--paragraph-font-size-medium);
+
+    @keyframes bg-fade-in {
+        from {
+            background-color: var(--bg-color-medium);
+            text-decoration: underline solid transparent;
+        }
+        to {
+            background-color: var(--bg-color-light);
+            text-decoration: underline solid var(--text-color-light);
+        }
+    }
+    @keyframes bg-fade-out {
+        from {
+            background-color: var(--bg-color-light);
+            text-decoration: underline solid var(--text-color-light);
+        }
+        to {
+            background-color: var(--bg-color-medium);
+            text-decoration: underline solid transparent;
+        }
+    }
+
     :hover {
-        text-decoration: underline !important;
+        text-decoration: underline solid var(--text-color-light);
+        animation-name: td-fade-in;
+        animation-duration: 0.2s;
     }
 `;
 
@@ -26,8 +50,12 @@ export const MobileNavItem = styled((props) => <NavItem {...props} />)`
     padding: 25px 0;
     border-top: 1px solid #555;
     margin: 0 !important;
+    animation-duration: 0.3s;
+    animation-name: bg-fade-out;
     :hover {
         background-color: var(--bg-color-light);
+        animation-name: bg-fade-in;
+        animation-duration: 0.3s;
     }
     :nth-last-child(2) {
         border-bottom: 1px solid var(--bg-color-extra-light) !important;

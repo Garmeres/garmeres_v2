a4c4f3f3741ed6056b30dae92ffc77cffe3b3f42
Levi Sørum <levi@kraftlauget.no>
2023-04-27 06:42:35 +0200

    style: 💄 Blog browser responsiveness for mobile
    ✅ Closes: #25

--- src/components/page-components/blog-browser/blog-post-thumbnail.js
+++ src/components/page-components/blog-browser/blog-post-thumbnail.js
@@ -1,34 +1,38 @@
-import React from "react";
-import styled from "styled-components";
-import variables from "../../../styles/variables";
+import React from 'react';
+import styled from 'styled-components';
+import variables from '../../../styles/variables';
 
 const BlogPostThumbnail = styled((props) => {
-    const thumbnail =
-        props.node.content.thumbnail.filename !== ""
-            ? props.node.content.thumbnail
-            : props.source.default_thumbnail;
-    return (
-        <img
-            {...props}
-            src={thumbnail.filename}
-            alt={thumbnail.alt}
-            title={thumbnail.copyright}
-        />
-    );
+	const thumbnail =
+		props.node.content.thumbnail.filename !== ''
+			? props.node.content.thumbnail
+			: props.source.default_thumbnail;
+	return (
+		<img
+			{...props}
+			src={thumbnail.filename}
+			alt={thumbnail.alt}
+			title={thumbnail.copyright}
+		/>
+	);
 })`
-    --blog-post-thumbnail-width: calc(
-        var(--blog-post-card-width) - calc(var(--blog-post-card-padding) * 2)
-    );
-    --blog-post-thumbnail-height: 350px;
-    min-width: var(--blog-post-thumbnail-width);
-    max-width: var(--blog-post-thumbnail-width);
-    min-height: var(--blog-post-thumbnail-height);
-    max-height: var(--blog-post-thumbnail-height);
-    box-sizing: border-box;
-    object-fit: cover;
-    @media screen and (max-width: ${variables.screenWidthSmall}) {
-        --blog-post-thumbnail-width: 100%;
-    }
+	--blog-post-thumbnail-width: calc(
+		var(--blog-post-card-width) - calc(var(--blog-post-card-padding) * 2)
+	);
+	--blog-post-thumbnail-height: 350px;
+	min-width: var(--blog-post-thumbnail-width);
+	max-width: var(--blog-post-thumbnail-width);
+	min-height: var(--blog-post-thumbnail-height);
+	max-height: var(--blog-post-thumbnail-height);
+	box-sizing: border-box;
+	object-fit: cover;
+	@media screen and (max-width: ${variables.screenWidthSmall}) {
+		--blog-post-thumbnail-width: 100%;
+	}
+	@media screen and (max-width: ${variables.screenWidthExtraSmall}) {
+		--blog-post-thumbnail-width: 85vw;
+		--blog-post-thumbnail-height: 85vw;
+	}
 `;
 
 export default BlogPostThumbnail;

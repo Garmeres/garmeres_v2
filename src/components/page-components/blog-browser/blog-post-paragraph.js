bc3e1e55d4dfb4e0d72080a090404e72165092f8
Levi Sørum <levi@kraftlauget.no>
2023-05-11 13:38:40 +0200

    style: 💄 Blog browser responsiveness
    ✅ Closes: #40

--- src/components/page-components/blog-browser/blog-post-paragraph.js
+++ src/components/page-components/blog-browser/blog-post-paragraph.js
@@ -1,17 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-import { bloksToText } from "../../../../helpers/storyblok-helpers/blok-to-text";
+import React from 'react';
+import styled from 'styled-components';
+import { bloksToText } from '../../../../helpers/storyblok-helpers/blok-to-text';
 
 const BlogPostParagraph = styled((props) => {
-    const blogPostText = bloksToText(props.node.content.body);
-    return (
-        <p {...props}>
-            {blogPostText.substring(0, 100).trim()}
-            {blogPostText.length > 100 ? "..." : null}
-        </p>
-    );
+	const blogPostText = bloksToText(props.node.content.body);
+	const paragraphLength = 200;
+	return (
+		<p {...props}>
+			{blogPostText.substring(0, paragraphLength).trim()}
+			{blogPostText.length > paragraphLength ? '...' : null}
+		</p>
+	);
 })`
-    margin-bottom: 12pt;
+	font-size: 1em;
+	line-height: 1.5em;
+	margin: 1em 0;
+	font-family: Bellota Text;
 `;
 
 export default BlogPostParagraph;
--- src/components/page-components/blog-browser/blog-post-title.js
+++ src/components/page-components/blog-browser/blog-post-title.js
@@ -1,17 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const BlogPostTitle = styled((props) => {
-    let title =
-        props.node.lang !== "default"
-            ? props.node.translated_slugs.find(
-                  (item) => item.lang === props.node.lang
-              ).name
-            : props.node.name;
-    if (title == null) {
-        title = props.node.name;
-    }
-    return <h2 {...props}>{title}</h2>;
-})``;
+	let title =
+		props.node.lang !== 'default'
+			? props.node.translated_slugs.find(
+					(item) => item.lang === props.node.lang
+			  ).name
+			: props.node.name;
+	if (title == null) {
+		title = props.node.name;
+	}
+	return <h2 {...props}>{title}</h2>;
+})`
+	font-size: 1.2em;
+	margin: 1.2em 0;
+	font-family: Bellota Text;
+`;
 
 export default BlogPostTitle;
